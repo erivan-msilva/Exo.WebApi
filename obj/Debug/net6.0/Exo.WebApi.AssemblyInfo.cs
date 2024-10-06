d84473ce85822c83252f1d696fd8fa972e253a57
erivan.mendes <contatoerivan.ms@gmail.com>
2024-10-05 22:17:24 -0300

    vs3

--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52b29f238a625d8fa48f10619cd22ba9a1392776")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b1c02e4fc0f953b880e4bf1624e53377e2e9df3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
